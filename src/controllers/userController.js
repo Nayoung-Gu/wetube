0fa5c2d1ea61440cdba28604b6b4e09c8ca4f67d
Nayoung-Gu <80025366+Nayoung-Gu@users.noreply.github.com>
2022-01-10 21:02:42 +0900

    Added profile-edit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -31,8 +31,11 @@ export const postJoin = async (req, res) => {
 export const getEdit = (req, res) => {
     return res.render("edit-profile", {pageTitle: "Edit Profile"})
 }
-export const postEdit = (req, res) => {
-    return res.render("edit-profile");
+export const postEdit = async (req, res) => {
+    const {session: {user: {_id},}, body: {name, email, username, location},} = req;
+    const updatedUser = await User.findByIdAndUpdate(_id,{name, email, username, location}, {new:true});
+    req.session.user = updatedUser;
+    return res.redirect("/users/edit");
 };
 export const getLogin = (req, res) => res.render("login", {pageTitle: "Login"});
 export const postLogin = async (req, res) => {
