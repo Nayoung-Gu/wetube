686ac9e4ca3ef0bc9041238a86c07b9a7c9166c5
Nayoung-Gu <80025366+Nayoung-Gu@users.noreply.github.com>
2022-01-27 22:51:41 +0900

    some format changed

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import "regenerator-runtime";
 import "dotenv/config";
 import "./db";
 import "./models/Video";
@@ -7,6 +8,7 @@ import app from "./server";
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
